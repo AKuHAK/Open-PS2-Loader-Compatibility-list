ffafcfda2d2e4fa1f21a6a94aeef2d8758093b56
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2022-06-29 17:28:55 +0300

    test

--- script/ProcessData.sh
+++ script/ProcessData.sh
@@ -3,23 +3,23 @@ rm -rf *.md
 rm -rf *.JSON
 rm -rf *.MDUMMY
 echo Downloading Report number "$1"
-wget -q --show-progress "https://api.github.com/repos/ps2homebrew/Open-PS2-Loader-Compatibility-list/issues/$1" -O REPORT.JSON
+wget -q --show-progress "https://api.github.com/repos/$2/issues/$1" -O REPORT.JSON
 dos2unix REPORT.JSON
 echo Processing data...
 jq -r '.body' REPORT.JSON > BODY.MDUMMY
 awk '/^### */{ close(out); out=$2".md" } out!=""{print > out}' BODY.MDUMMY
-read -p "Press any key to resume ..."
+
 echo MDs
 ls *.md
 echo END MDs
-read -p "Press any key to resume ..."
+
 for a in *.md
 do
 sed -i '/^###/d' "$a"
 sed -i '/^\s*$/d' "$a"
 sed -i '/\/n$/d;' "$a"
 done
-read -p "Press any key to resume ..."
+
 declare TESTERNAME=$(jq .user.login REPORT.JSON)
 declare TESTERURL=$(jq .user.html_url REPORT.JSON)
 declare TESTER="[${TESTERNAME}](${TESTERURL})"
@@ -37,21 +37,21 @@ declare OPLBETA=$(head -n 1 BETA.md)
 declare CONSOLE_MODEL=$(head -n 1 Console.md)
 declare FORMAT=$(head -n 1 Image.md)
 declare MEDIA=$(head -n 1 Media.md)
-read -p "Press any key to resume ..."
+
 if grep -q "_No response_" Compatibility.md
 then
 	echo - No compatibility modes provided
 else
 	declare COMPAT_MODES=$(paste -s -d ' ' Compatibility.md)
 fi
-read -p "Press any key to resume ..."
+
 if grep -q "_No response_" comments.md
 then
 	echo - No comments provided
 else
 	declare COMMENTS=$(paste -s -d ' ' comments.md)
 fi
-read -p "Press any key to resume ..."
+
 sed -ni '/\[X\]/p' features.md
 for A in VMC PADEMU PADMACRO IGR GSM
 do
@@ -79,7 +79,7 @@ echo GSM 			- "$GSM" -
 echo ---
 echo COMPAT_MODES 	- "$COMPAT_MODES" -
 echo COMMENTS 		- "$COMMENTS" -
-read -p "Press any key to resume ..."
+
 if [[ "$TITLE" =~ ^[a-z]|[A-Z].* ]]; then
 FSTCHAR=${TITLE:0:1}
 FSTCHAR=${FSTCHAR^^}
